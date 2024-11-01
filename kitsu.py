6f5b9c0bfabfd8a1f987b4c76a3c4fd79b256dfb
DiogoMiguel93 <105163611+DiogoMiguel93@users.noreply.github.com>
2024-11-01 10:05:36 +0100

    Add expire time to kitsu_id saved as imdb_id

--- kitsu.py
+++ kitsu.py
@@ -1,4 +1,5 @@
 from diskcache import Cache
+from datetime import timedelta
 import httpx
 import anime_mapping
 
@@ -6,6 +7,7 @@ kitsu_addon_url = 'https://anime-kitsu.strem.fun'
 
 # Cache load
 kitsu_cache = Cache('/tmp/kitsu_ids')
+cache_expire_time = timedelta(days=7).total_seconds()
 kitsu_cache.clear()
 imdb_map = anime_mapping.load()
 for kitsu_id, imdb_id in imdb_map.items():
@@ -20,13 +22,10 @@ async def convert_to_imdb(kitsu_id: str, type: str) -> str:
             response = await client.get(f"{kitsu_addon_url}/meta/{type}/{kitsu_id.replace(':','%3A')}.json")
             try:
                 imdb_id = response.json()['meta']['imdb_id']
-
+                kitsu_cache.set(kitsu_id, imdb_id)
             except:
                 # If imdb_id not found save kitsu_id as imdb_id (better performance)
-                imdb_id = kitsu_id
+                kitsu_cache.set(kitsu_id, kitsu_id, expire=cache_expire_time)
                 return kitsu_id
-            
-            finally:
-                kitsu_cache.set(kitsu_id, imdb_id)
 
     return imdb_id
