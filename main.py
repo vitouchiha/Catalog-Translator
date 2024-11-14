25975c049928634248448c10dd851925e62894d9
DiogoMiguel93 <105163611+DiogoMiguel93@users.noreply.github.com>
2024-11-14 20:51:01 +0100

    add cache meta

--- main.py
+++ main.py
@@ -206,6 +206,7 @@ async def get_meta(addon_url, type: str, id: str):
                     response = await client.get(f"{kitsu.kitsu_addon_url}/meta/{type}/{id.replace(':','%3A')}.json")
                     meta = response.json()
 
+        tmp_cache.set(id, meta, expire=cache_expire_time)
         meta['meta']['id'] = id
         return meta
 
