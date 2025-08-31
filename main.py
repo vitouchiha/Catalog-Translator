28b3233b49fc97ebe0fbe61fa557a1e9738a3335
DiogoMiguel93 <105163611+DiogoMiguel93@users.noreply.github.com>
2025-08-31 20:05:10 +0200

    Cache test beamup

--- main.py
+++ main.py
@@ -80,6 +80,7 @@ def json_response(data):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "0"
+    response.headers["Surrogate-Control"] = "no-store"
     return response
 
 
